6e62c9cc24b03a650dc2aab65b80e9b13d1367a1
AZIZBEKRAXMONBERDIYEV <jxechkim079@gmail.com>
2023-01-10 10:57:36 +0500

    cod07

--- Index07.py
+++ Index07.py
@@ -2,8 +2,13 @@ def main(s,n):
     """
     The s string variable is given. n Return the character in the index, otherwise return False.
     Args:
-        s(str): parameter
-    Returns:
-        str: answer
-    """
-    return
+    s(str): parameter
+     """
+    d=len(s)
+    if d>=n:
+       c= s[0:n]
+    else:
+       c= False
+
+    return c
+print(main("jbadhb",9))
